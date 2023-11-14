8396ff1d9491d846e7d3fe390832efdd132efcf8
Frank Harvy Astaiza Oviedo <125573221+HarvyAstaiza@users.noreply.github.com>
2023-11-13 20:28:19 -0500

    Add files via upload

---
+++ Controladores/ControladorResultado.py
@@ -0,0 +1,62 @@
+from Modelos.Resultado import Resultado
+from Modelos.Mesa import Mesa
+from Modelos.Candidato import Candidato
+from Modelos.Partido import Partido
+from Repositorios.RepositorioResultado import RepositorioResultado
+from Repositorios.RepositorioMesa import RepositorioMesa
+from Repositorios.RepositorioCandidato import RepositorioCandidato
+from Repositorios.RepositorioPartido import RepositorioPartido
+"""
+Dentro de la clase se crean unos metodos, estos serán los encargados de manipular
+a los modelos, en estos se programarán las tareas básicas tales como crear, listar,
+visualizar, modificar y eliminar. (CRUD)
+"""
+
+
+class ControladorResultado():
+    """
+    constructor que permite llevar a cabo la creacion de instancias del controlador.
+    """
+
+    def __init__(self):
+        self.repositorioResultado = RepositorioResultado()
+        self.repositorioCandidato = RepositorioCandidato()
+        self.repositorioPartido=RepositorioPartido()
+        self.repositorioMesa = RepositorioMesa()
+        print("Creando ControladorResultado")
+
+    def index(self):
+        print("Listar todas los Resultados")
+        return self.repositorioResultado.findAll()
+
+    def create(self, infoResultado,id_partido, id_candidato, id_mesa):
+        nuevaResultado = Resultado(infoResultado)
+        elPartido=Partido(self.repositorioPartido.findById(id_partido))
+        elCandidato = Candidato(self.repositorioCandidato.findById(id_candidato))
+        laMesa = Mesa(self.repositorioMesa.findById(id_mesa))
+        nuevaResultado.partido=elPartido
+        nuevaResultado.candidato = elCandidato
+        nuevaResultado.mesa = laMesa
+
+        return self.repositorioResultado.save(nuevaResultado)
+
+    def show(self, id):
+        elResultado = Resultado(self.repositorioResultado.findById(id))
+        return elResultado.__dict__
+
+    """
+    Modificación de inscripción (estudiante y materia)  
+    """
+
+    def update(self, id, infoResultado, id_candidato, id_mesa):
+        laResultado = Resultado(self.repositorioResultado.findById(id))
+        laResultado.numero_mesa = infoResultado["numero_mesa"]
+        laResultado.id_partido = infoResultado["id_partido"]
+        elCandidato = Candidato(self.repositorioCandidato.findById(id_candidato))
+        laMesa = Mesa(self.repositorioMesa.findById(id_mesa))
+        laResultado.candidato = elCandidato
+        laResultado.mesa = laMesa
+        return self.repositorioResultado.save(laResultado)
+
+    def delete(self, id):
+        return self.repositorioResultado.delete(id)
